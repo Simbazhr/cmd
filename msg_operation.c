120a44e1346143fc0cc3af8267e822e30fd8a787
Simbazhr <simbazhr@foxmail.com>
2016-12-29 14:42:36 +0800

    Update msg_operation.c

--- msg_operation.c
+++ msg_operation.c
@@ -4,20 +4,24 @@
 
 #include "msg_operation.h"
 #include "string.h"
-int msg_del(struct msg_wrapper *msg){
+int msg_del(struct msg_wrapper *msg)
+{
     memset(msg->text,0,BUFFER_SIZE);
     msg->type=1;
     return 1;
 }
-int msg_add(struct msg_wrapper *msg, char *string) {
+int msg_add(struct msg_wrapper *msg, char *string)
+{
     strcat(msg->text," ");
     strcat(msg->text, string);
     return 1;
 }
-int msg_cmd(struct msg_wrapper *msg, char *string){
+int msg_cmd(struct msg_wrapper *msg, char *string)
+{
     strcat(msg->text,string);
     return 1;
 }
-int msg_end(struct msg_wrapper *msg){
+int msg_end(struct msg_wrapper *msg)
+{
     strcat(msg->text,"\0");
 }
